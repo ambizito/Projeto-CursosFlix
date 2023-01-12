9de5df2890edc559a073d12135360692889ec41a
André Azevedo <aazevedo525@gmail.com>
2023-01-12 22:27:35 +0100

    inicio do projeto

---
+++ config/sequelizeCli.js
@@ -0,0 +1,11 @@
+module.exports = {
+    development: {
+        dialect: 'postgres'
+        host: 'localhost',
+        port: 5432,
+        database: 'CursoFlix_dev',
+        username: 'CursoFlix',
+        password: 'CursoFlix',
+    
+    }
+}
